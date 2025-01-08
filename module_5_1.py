c066186ac9de2311e0a057baf6719307a21eb435
Alxndr32947 <alexsh32947@gmail.com>
2025-01-08 22:18:04 +0300

    08/01/25

--- module_5_1.py
+++ module_5_1.py
@@ -1,29 +1,29 @@
+# module_5_1
 class House:
-    """
-    Класс для представления здания.
-    Содержит название здания, количество этажей и возможность перехода на этаж.
-    """
     def __init__(self, name, number_of_floors):
         """
         Инициализация объекта класса House.
-
-        :param name: Название дома (строка)
-        :param number_of_floors: Количество этажей (целое число)
+        :param name: Название здания.
+        :param number_of_floors: Количество этажей.
         """
         self.name = name
         self.number_of_floors = number_of_floors
 
     def go_to(self, new_floor):
+        """
+        Метод для перехода на указанный этаж.
+        :param new_floor: Этаж, на который нужно перейти.
+        """
         if 1 <= new_floor <= self.number_of_floors:
             print(*range(1, new_floor + 1), sep="\n")
         else:
             print("Такого этажа не существует")
 
 
-# Примеры использования, которые выполняются только при запуске модуля напрямую
+# Примеры использования
 if __name__ == "__main__":
-    house1 = House("ЖК Эльбрус", 30)
-    house2 = House("Коттедж", 2)
+    h1 = House('ЖК Горский', 18)
+    h2 = House('Домик в деревне', 2)
 
-    house1.go_to(5)  # Вывод этажей от 1 до 5
-    house2.go_to(10)  # Сообщение о недопустимом этаже
+    h1.go_to(5)
+    h2.go_to(10)
